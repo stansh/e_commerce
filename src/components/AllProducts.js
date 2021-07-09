0b8033083e1b2f1fbadd9121d4121bc2e5bc4bc2
Stan S <stan.shm@gmail.com>
2021-07-09 18:19:30 -0500

    more code on the component

--- src/components/AllProducts.js
+++ src/components/AllProducts.js
@@ -1,10 +1,68 @@
 
+import React,{useState,useEffect}from "react";
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button
+  } from 'reactstrap';
+
 const url = 'https://fakestoreapi.com/products';
 
 
+function AllProducts() {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [products, setItems] = useState([]);
+  
+    // Note: the empty deps array [] means
+    // this useEffect will run once
+    // similar to componentDidMount()
+    useEffect(() => {
+      fetch(url)
+        .then(res => res.json())
+        .then(
+          (result) => {
+            setIsLoaded(true);
+            setItems(result);
+          },
+          // Note: it's important to handle errors here
+          // instead of a catch() block so that we don't swallow
+          // exceptions from actual bugs in components.
+          (error) => {
+            setIsLoaded(true);
+            setError(error);
+          }
+        )
+    }, [])
+  
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        
+            <div className ='row justify-content-center'>
+          {products.map(product => (
+               
+              
+            <Card className ='col-md-3 mx-2' key ={product.id}>
+            <h3>{product.title}</h3>
+            <h3>{product.price}</h3>
+            <p>{product.description}</p>
+            </Card >
+          ))}
+          </div>
+          
+      
+      );
+    }
+  }
 
 
-function AllProducts () {
+
+
+
+/* function AllProducts () {
     fetch(url)
     .then(res => res.json())
     .then(res => {   
@@ -25,7 +83,7 @@ function AllProducts () {
     });
          
         
-}
+} */
 
 export default AllProducts;
     
